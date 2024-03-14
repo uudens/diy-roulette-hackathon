9af8204ba84e152699c4d75f581267038923c863
Juris <jurisk@users.noreply.github.com>
2024-03-14 13:17:38 +0200

    Flask example to show OpenCV video feed

--- ReadMe.md
+++ ReadMe.md
@@ -3,5 +3,6 @@ python3 -m venv venv
 . venv/bin/activate
 pip install numpy
 pip install opencv-python
+pip install Flask
 python blue-detector.py
 ```
--- blue-detector.py
+++ blue-detector.py
@@ -27,35 +27,37 @@ def read_frame(capture):
     if is_raspberry_pi():
         return capture.capture_array()
     else:
+        # ret will return a true value if the frame exists otherwise False
         ret, frame = capture.read()
         return frame
 
 
-capture = create_capture()
+if __name__ == '__main__':
+    capture = create_capture()
 
-while True:
-    frame = read_frame(capture)
-    # ret will return a true value if the frame exists otherwise False
-    into_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # changing the color format from BGr to HSV
-    # This will be used to create the mask
-    L_limit = np.array([98, 50, 50])  # setting the blue lower limit
-    U_limit = np.array([139, 255, 255])  # setting the blue upper limit
+    while True:
+        frame = read_frame(capture)
 
-    b_mask = cv2.inRange(into_hsv, L_limit, U_limit)
-    # creating the mask using inRange() function
-    # this will produce an image where the color of the objects
-    # falling in the range will turn white and rest will be black
-    blue=cv2.bitwise_and(frame, frame, mask=b_mask)
-    # this will give the color to mask.
-    cv2.imshow('Original', frame) # to display the original frame
-    cv2.imshow('Blue Detector', blue) # to display the blue object output
+        into_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # changing the color format from BGr to HSV
+        # This will be used to create the mask
+        L_limit = np.array([98, 50, 50])  # setting the blue lower limit
+        U_limit = np.array([139, 255, 255])  # setting the blue upper limit
 
-    if cv2.waitKey(1) == 27:
-        break
-    # this function will be triggered when the ESC key is pressed
-    # and the while loop will terminate and so will the program
+        b_mask = cv2.inRange(into_hsv, L_limit, U_limit)
+        # creating the mask using inRange() function
+        # this will produce an image where the color of the objects
+        # falling in the range will turn white and rest will be black
+        blue=cv2.bitwise_and(frame, frame, mask=b_mask)
+        # this will give the color to mask.
+        cv2.imshow('Original', frame) # to display the original frame
+        cv2.imshow('Blue Detector', blue) # to display the blue object output
 
-capture.release()
+        if cv2.waitKey(1) == 27:
+            break
+        # this function will be triggered when the ESC key is pressed
+        # and the while loop will terminate and so will the program
 
-cv2.destroyAllWindows()
+    capture.release()
+
+    cv2.destroyAllWindows()
