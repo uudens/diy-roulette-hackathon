0411534e53d73974ed443844b7560058a3886c0e
dvsmc <112554734+dvsmc@users.noreply.github.com>
2024-03-14 22:45:48 +0200

    Preview recorder file

---
+++ previewrecorder.sh
@@ -0,0 +1,1 @@
+libcamera-vid -o test.h264 -t 30000 --width 1280 --height 720 --framerate 60 --codec h264 --profile high --quality 100 --intra 5 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast
