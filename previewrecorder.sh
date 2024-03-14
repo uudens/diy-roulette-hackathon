a92d52bf77d0a28ab9f52a37bbbcce016b5e93be
dvsmc <112554734+dvsmc@users.noreply.github.com>
2024-03-15 00:01:11 +0200

    Preview recorder file

--- previewrecorder.sh
+++ previewrecorder.sh
@@ -1,2 +1,4 @@
 libcamera-vid -o test.h264 -t 30000 --width 1280 --height 720 --framerate 60 --codec h264 --profile high --quality 100 --intra 5 --autofocus-mode continuous --autofocus-range full --autofocus-speed fast
-ffmpeg -framerate 60 -i test.h264 -c copy test.mp4
+ffmpeg -framerate 60 -y -i test.h264 -c copy test.mp4
+rm test.h264
+echo "Saved as test.mp4"
